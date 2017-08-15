a3c6b6390184f3cefe9ec784b491e417a4e8ea81
Anna Bush <annabush092@gmail.com>
2017-08-15 18:32:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size>1
+      array[array.size-1] = "and "+array[array.size-1]
+  end
+  if array.size>2
+    array.join(", ")
+  else
+    array.join(" ")
+  end
 end
